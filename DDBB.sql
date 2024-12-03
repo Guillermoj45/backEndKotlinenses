05ef8cbf75fa231de11c00e38aa5bc8858e37d54
Guillermo <guillermoremesa@gmail.com>
2024-12-03 12:58:12 +0100

    Esquema actualizado

--- DDBB.sql
+++ DDBB.sql
@@ -1,8 +1,9 @@
+
 create table usuario
 (
     id        serial
         primary key,
-    avatar    varchar(100)
+    avatar    varchar(500)
         unique,
     username  varchar(45) not null,
     email     varchar(300),
@@ -15,17 +16,22 @@ create table usuario
 alter table usuario
     owner to postgres;
 
-create unique index username_users on usuario (username)
+create unique index username_users
+    on usuario (username)
     where (eliminado = false);
 
-create unique index email_users on usuario (email)
+create unique index email_users
+    on usuario (email)
     where (eliminado = false);
 
-create index users_deleted on usuario (eliminado);
+create index users_deleted
+    on usuario (eliminado);
 
-create index users_password on usuario (password);
+create index users_password
+    on usuario (password);
 
-create index user_password on usuario (password);
+create index user_password
+    on usuario (password);
 
 create table datos_personales
 (
@@ -41,32 +47,28 @@ create table datos_personales
     direccion        varchar(150)
 );
 
-
 create table cuenta
 (
-    id          serial
+    id           serial
         primary key,
-    nombre      varchar(20)  not null,
-    descripcion varchar(400),
-    imagen      varchar(200) not null
-        unique,
-    imagenfondo varchar(200)
-        unique
+    nombre       varchar(20)  not null,
+    descripcion  varchar(400),
+    imagen       varchar(200) not null,
+    imagen_fondo varchar(200)
 );
-
 
 create table amigo
 (
-    id serial primary key not null,
     usuario1  integer               not null
         constraint users_usuario1
             references usuario,
     usuario2  integer               not null
         constraint users_usuario2
             references usuario,
-    confimado boolean default false not null
+    confimado boolean default false not null,
+    id        serial
+        primary key
 );
-
 
 create table user_cuenta
 (
@@ -74,13 +76,14 @@ create table user_cuenta
         primary key,
     id_user   integer not null
         constraint user_user_cuentas
-            references usuario,
+            references usuario
+            on update cascade on delete cascade,
     id_cuenta integer not null
         constraint cuentas_user_cuentas
-            references cuenta,
+            references cuenta
+            on update cascade on delete cascade,
     is_admin  boolean not null
 );
-
 
 create table producto
 (
@@ -89,47 +92,31 @@ create table producto
     nombre      varchar(100) not null,
     precio      numeric(8, 2),
     descripcion varchar(400),
-    imagen      varchar(200)
-        unique,
+    imagen      varchar(200),
     id_cuenta   integer      not null
         constraint cuenta_producto
-            references cuenta,
+            references cuenta
+            on update cascade on delete cascade,
     id_users    integer      not null
         constraint user_producto
             references usuario
+            on update cascade on delete cascade,
+    fecha       timestamp default now(),
+    facturas    varchar(200)
 );
-
-
-create table producto_usuario_cuenta
-(
-    id serial primary key not null,
-    id_user     integer not null
-        constraint productos_productos_user
-            references usuario,
-    id_producto integer not null
-        constraint users_productos_user
-            references producto,
-    id_cuenta   integer not null
-        constraint productos_cuenta_user
-            references cuenta,
-    pagado      boolean,
-    fecha       timestamp(6)
-);
-
 
 create table historial_pago
 (
-    id          serial
+    id        serial
         primary key,
-    tipo_pago   smallint not null,
-    id_users    integer  not null
+    tipo_pago smallint not null,
+    id_users  integer  not null
         constraint users_historial_pagos
             references usuario,
-    id_producto integer  not null
-        constraint productos_historial_pagos
-            references producto
+    id_cuenta integer
+        references cuenta,
+    monton    double precision
 );
-
 
 create table notificacion
 (
@@ -157,5 +144,7 @@ create table chat
     hora        timestamp default CURRENT_TIMESTAMP not null,
     mensaje     text
 );
+
+
 
 
