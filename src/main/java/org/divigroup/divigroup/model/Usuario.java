2d433ade5b806b2f718e5f697a88403b06629a2a
AdrianJavier7 <aperezmiranda@safareyes.es>
2024-12-08 09:34:04 +0100

    ESTE SI

--- src/main/java/org/divigroup/divigroup/model/Usuario.java
+++ src/main/java/org/divigroup/divigroup/model/Usuario.java
@@ -9,7 +9,7 @@ import org.divigroup.divigroup.model.enums.TipoPago;
 import org.hibernate.mapping.Set;
 
 @Entity
-@Table(name = "usuario", schema = "proyectokotlin")
+@Table(name = "usuario", schema = "divigroup")
 @Getter
 @Setter
 @AllArgsConstructor
